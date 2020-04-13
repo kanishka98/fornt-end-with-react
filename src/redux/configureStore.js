400d934bcab9a95f9319032be1486c108ec5c273
Kanishka Gupta <kanishkagupta576@gmail.com>
2020-04-13 14:55:24 +0530

    Fetch Handling Errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import { createForms } from 'react-redux-form'
-import { InitialFeedback } from './forms';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -15,12 +15,10 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
+            ...createForms({feedback: InitialFeedback})
         }),
         applyMiddleware(thunk, logger)
     );
 
     return store;
-}
+};
